4cf0e0d21f1472ca678a9dcdf3627aeddf0a5dfd
Mariana Godoy <marianagsilva@usp.br>
2021-02-26 17:11:07 -0300

    terceiro capítulo do curso OO em Go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/banco
+
+go 1.15
--- main.go
+++ main.go
@@ -9,6 +9,16 @@ type ContaCorrente struct {
 	saldo         float64
 }
 
+func (c *ContaCorrente) Sacar(valorSaque float64) string {
+	podeSacar := valorSaque > 0 && valorSaque <= c.saldo
+	if podeSacar {
+		c.saldo -= valorSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "Saldo insuficiente"
+	}
+}
+
 func main() {
 
 	var contaGuilherme ContaCorrente = ContaCorrente{ //contaGuilherme := ContaCorrente{}
@@ -17,8 +27,38 @@ func main() {
 		numeroConta:   12345,
 		saldo:         125.5,
 	}
-	contaBruna := ContaCorrente{"Bruna", 222, 1112223, 200} // mesma coisa que acima, declaração curta
 
-	fmt.Println(contaGuilherme)
-	fmt.Println(contaBruna)
+	var contaGuilherme2 ContaCorrente = ContaCorrente{ //contaGuilherme := ContaCorrente{}
+		titular:       "Guilherme",
+		numeroAgencia: 580,
+		numeroConta:   12345,
+		saldo:         125.5,
+	}
+	fmt.Println(contaGuilherme == contaGuilherme2)
+
+	contaBruna := ContaCorrente{"Bruna", 222, 1112223, 200} // mesma coisa que acima, declaração curta
+	contaBruna2 := ContaCorrente{"Bruna", 222, 1112223, 200}
+
+	fmt.Println(contaBruna == contaBruna2)
+
+	var contaCris *ContaCorrente
+	contaCris = new(ContaCorrente)
+	contaCris.titular = "Cris"
+	contaCris.saldo = 500
+
+	var contaCris2 *ContaCorrente
+	contaCris2 = new(ContaCorrente)
+	contaCris2.titular = "Cris"
+	contaCris2.saldo = 500
+
+	fmt.Println(*contaCris == *contaCris2)
+	fmt.Println(contaCris2)
+
+	contaSilvia := ContaCorrente{}
+	contaSilvia.titular = "Silvia"
+	contaSilvia.saldo = 500
+
+	fmt.Println(contaSilvia.saldo)
+	fmt.Println(contaSilvia.Sacar(300))
+
 }
