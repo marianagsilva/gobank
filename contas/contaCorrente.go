13ee1b3231bc7af7d192e1bbbd2005cd47d37e25
Mariana Godoy <marianagsilva@usp.br>
2021-03-01 16:21:32 -0300

    quarto capítulo do curso de OO em GO

---
+++ contas/contaCorrente.go
@@ -0,0 +1,39 @@
+package contas
+
+type ContaCorrente struct {
+	Titular       string
+	NumeroAgencia int
+	NumeroConta   int
+	Saldo         float64
+}
+
+func (c *ContaCorrente) Sacar(valorSaque float64) string {
+	podeSacar := valorSaque > 0 && valorSaque <= c.Saldo
+	if podeSacar {
+		c.Saldo -= valorSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "Saldoinsuficiente"
+	}
+}
+
+func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
+	if valorDoDeposito > 0 {
+		c.Saldo += valorDoDeposito
+		return "Deposito realizado com sucesso", c.Saldo
+	} else {
+		return "Valor do desposito menor do que 0", c.Saldo
+	}
+
+}
+
+func (c *ContaCorrente) Transferir(valorDaTransferencia float64, contaDestino *ContaCorrente) bool {
+	if valorDaTransferencia < c.Saldo && valorDaTransferencia > 0 {
+		c.Saldo -= valorDaTransferencia
+		contaDestino.Depositar(valorDaTransferencia)
+		return true
+	} else {
+		return false
+	}
+
+}
--- main.go
+++ main.go
@@ -1,64 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-type ContaCorrente struct {
-	titular       string
-	numeroAgencia int
-	numeroConta   int
-	saldo         float64
-}
-
-func (c *ContaCorrente) Sacar(valorSaque float64) string {
-	podeSacar := valorSaque > 0 && valorSaque <= c.saldo
-	if podeSacar {
-		c.saldo -= valorSaque
-		return "Saque realizado com sucesso"
-	} else {
-		return "Saldo insuficiente"
-	}
-}
+	"github.com/banco/contas"
+)
 
 func main() {
 
 	var contaGuilherme ContaCorrente = ContaCorrente{ //contaGuilherme := ContaCorrente{}
-		titular:       "Guilherme",
-		numeroAgencia: 589,
-		numeroConta:   12345,
-		saldo:         125.5,
+		Titular:       "Guilherme",
+		NumeroAgencia: 589,
+		NumeroConta:   12345,
+		Saldo:         125.5,
 	}
 
 	var contaGuilherme2 ContaCorrente = ContaCorrente{ //contaGuilherme := ContaCorrente{}
-		titular:       "Guilherme",
-		numeroAgencia: 580,
-		numeroConta:   12345,
-		saldo:         125.5,
+		Titular:       "Guilherme",
+		NumeroAgencia: 580,
+		NumeroConta:   12345,
+		Saldo:         125.5,
 	}
 	fmt.Println(contaGuilherme == contaGuilherme2)
 
-	contaBruna := ContaCorrente{"Bruna", 222, 1112223, 200} // mesma coisa que acima, declaração curta
-	contaBruna2 := ContaCorrente{"Bruna", 222, 1112223, 200}
+	contaBruna := contas.ContaCorrente{"Bruna", 222, 1112223, 200} // mesma coisa que acima, declaração curta
+	contaBruna2 := contas.ContaCorrente{"Bruna", 222, 1112223, 200}
 
 	fmt.Println(contaBruna == contaBruna2)
 
 	var contaCris *ContaCorrente
 	contaCris = new(ContaCorrente)
-	contaCris.titular = "Cris"
-	contaCris.saldo = 500
+	contaCris.Titular = "Cris"
+	contaCris.Saldo = 500
 
 	var contaCris2 *ContaCorrente
 	contaCris2 = new(ContaCorrente)
-	contaCris2.titular = "Cris"
-	contaCris2.saldo = 500
+	contaCris2.Titular = "Cris"
+	contaCris2.Saldo = 500
 
 	fmt.Println(*contaCris == *contaCris2)
 	fmt.Println(contaCris2)
 
 	contaSilvia := ContaCorrente{}
 	contaSilvia.titular = "Silvia"
-	contaSilvia.saldo = 500
+	contaSilvia.Saldo = 500
 
 	fmt.Println(contaSilvia.saldo)
-	fmt.Println(contaSilvia.Sacar(300))
+	fmt.Println(contaSilvia.Depositar(2500))
+	//fmt.Println(contaSilvia.Sacar(300))
+
+	contaSilvia = ContaCorrente{titular: "Silvia", saldo: 300}
+	contaGustavo := ContaCorrente{titular: "Gustavo", saldo: 100}
+
+	status := contaSilvia.Transferir(200, &contaGustavo)
+
+	fmt.Println(" ")
+	fmt.Println("Transferência")
+	fmt.Println(status)
+	fmt.Println(contaSilvia)
+	fmt.Println(contaGustavo)
 
 }
